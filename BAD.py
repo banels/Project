65ec15df84e58a5de74d52f9d44ef4a8a8a880b9
cedarshupe-ship-it <cedar.shupe@gmail.com>
2025-11-28 06:53:15 -0700

    c

--- BAD.py
+++ BAD.py
@@ -1,18 +1,17 @@
 import pandas as pd
 import matplotlib as mt
-cap23 = pd.read_csv("BigAhhDati\Python-Project-Milanamos-data\CAP_US_US_2023-01_2023-12.csv")
-cap24 = pd.read_csv("BigAhhDati\Python-Project-Milanamos-data\CAP_US_US_2024-01_2024-12.csv")
-seg23 = pd.read_csv("BigAhhDati\Python-Project-Milanamos-data\SEG_F_US_US_2023-01_2023-12.csv")
-seg24 = pd.read_csv("BigAhhDati\Python-Project-Milanamos-data\SEG_F_US_US_2024-01_2024-12.csv")
+cap23 = pd.read_csv("Python-Project-Milanamos-data/CAP_US_US_2023-01_2023-12.csv")
+cap24 = pd.read_csv("Python-Project-Milanamos-data/CAP_US_US_2024-01_2024-12.csv")
+seg23 = pd.read_csv("Python-Project-Milanamos-data/SEG_F_US_US_2023-01_2023-12.csv")
+seg24 = pd.read_csv("Python-Project-Milanamos-data/SEG_F_US_US_2024-01_2024-12.csv")
 
-#change
+#phase 1:
+#Display top of each dataframe
+print(cap23.head())
+print(cap24.head())
+print(seg23.head())
+print(seg24.head())
 
-print(sum(seg24["seg_passengers"])-sum(seg23["seg_passengers"]))
-print(sum(seg24["seg_passengers"]))
-print(sum(seg23["seg_passengers"]))
+#Display descriptive stats
+print('Minimum: ' + str(seg24["seg_revenue_usd"].min()) + ' Maximum: ' + str(seg24["seg_revenue_usd"].max()) + ' Mean: ' + str(seg24["seg_revenue_usd"].mean()))
 
-print(max(seg24["seg_revenue_usd"]))
-print(min(seg24["seg_revenue_usd"]))
-
-seg24.sort_values("seg_revenue_usd",ascending=False,inplace=True)
-print(seg24["seg_revenue_usd"].head(10))
