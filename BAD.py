2c6872aa0be9b902707e0942320f4d41be56c3be
BASIL-NELSON <99857734+BASIL-NELSON@users.noreply.github.com>
2025-11-28 14:53:43 +0100

    test

--- BAD.py
+++ BAD.py
@@ -7,12 +7,3 @@ seg24 = pd.read_csv("BigAhhDati\Python-Project-Milanamos-data\SEG_F_US_US_2024-0
 
 #change
 
-print(sum(seg24["seg_passengers"])-sum(seg23["seg_passengers"]))
-print(sum(seg24["seg_passengers"]))
-print(sum(seg23["seg_passengers"]))
-
-print(max(seg24["seg_revenue_usd"]))
-print(min(seg24["seg_revenue_usd"]))
-
-seg24.sort_values("seg_revenue_usd",ascending=False,inplace=True)
-print(seg24["seg_revenue_usd"].head(10))
---
+++ Phase_TWO.py
@@ -0,0 +1,7 @@
+
+
+from pathlib import Path
+
+
+cwd = Path.cwd() # Current working directory
+print(cwd)
