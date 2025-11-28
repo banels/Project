2ec83dfa7052cf4ee4bf394b3cf7a0eb23bc4c2a
cedarshupe-ship-it <cedar.shupe@gmail.com>
2025-11-28 06:24:15 -0700

    cedar did a thing

--- BAD.py
+++ BAD.py
@@ -5,7 +5,7 @@ cap24 = pd.read_csv("BigAhhDati\Python-Project-Milanamos-data\CAP_US_US_2024-01_
 seg23 = pd.read_csv("BigAhhDati\Python-Project-Milanamos-data\SEG_F_US_US_2023-01_2023-12.csv")
 seg24 = pd.read_csv("BigAhhDati\Python-Project-Milanamos-data\SEG_F_US_US_2024-01_2024-12.csv")
 
-
+#change
 
 print(sum(seg24["seg_passengers"])-sum(seg23["seg_passengers"]))
 print(sum(seg24["seg_passengers"]))
