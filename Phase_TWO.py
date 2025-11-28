1fd874034da3e82b820e2d439ec955b9a7229d35
BASIL-NELSON <99857734+BASIL-NELSON@users.noreply.github.com>
2025-11-28 14:55:57 +0100

    ok

--- Phase_TWO.py
+++ Phase_TWO.py
@@ -5,3 +5,15 @@ from pathlib import Path
 
 cwd = Path.cwd() # Current working directory
 print(cwd)
+
+
+
+print(sum(seg24["seg_passengers"])-sum(seg23["seg_passengers"]))
+print(sum(seg24["seg_passengers"]))
+print(sum(seg23["seg_passengers"]))
+
+print(max(seg24["seg_revenue_usd"]))
+print(min(seg24["seg_revenue_usd"]))
+
+seg24.sort_values("seg_revenue_usd",ascending=False,inplace=True)
+print(seg24["seg_revenue_usd"].head(10))
