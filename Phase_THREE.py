0d970b57d552902c7b82144d2b56c8208d735ce0
BASIL-NELSON <99857734+BASIL-NELSON@users.noreply.github.com>
2025-11-28 16:19:21 +0100

    end of class

--- Phase_THREE.py
+++ Phase_THREE.py
@@ -12,12 +12,12 @@ seg24 = pd.read_csv("Python-Project-Milanamos-data/SEG_F_US_US_2024-01_2024-12.c
 # seg24.sort_values("seg_revenue_usd",ascending=False,inplace=True)
 
 # merge the two lists and match corresponding routes and airlines and dates
-merged23 = pd.merge(cap23, seg23, how="left", left_on=['origin', 'destination', 'operating_airline', 'year', 'month'], right_on=['leg_origin', 'leg_destination', 'operating_airline', 'year', 'month'])
+merged24 = pd.merge(cap24, seg24, how="left", left_on=['origin', 'destination', 'year', 'month'], right_on=['leg_origin', 'leg_destination', 'year', 'month'])
 # Calculate load factor
-merged23['load_factor'] = merged23['seg_passengers'] / merged23['capacity']
+merged24['load_factor'] = merged24['seg_passengers'] / merged24['capacity']
 
-
+merged24 = pd.DataFrame(cap23.groupby())
 #this is incomplete
-merged23.sort_values("load_factor",ascending=True,inplace=True)
-
-print(merged23['load_factor'].head)
+merged24.sort_values("load_factor",ascending=False,inplace=True)
+print(len(merged24))
+print(len(merged24[merged24['load_factor'] > 1.2]))
