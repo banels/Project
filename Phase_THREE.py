b4cc208427cbb3146338a77d7276f94c54b5c920
BASIL-NELSON <99857734+BASIL-NELSON@users.noreply.github.com>
2025-12-05 10:52:01 +0100

    cleaned some shi

--- Phase_THREE.py
+++ Phase_THREE.py
@@ -6,19 +6,24 @@ import matplotlib as mt
 
 cap23 = pd.read_csv("Python-Project-Milanamos-data/CAP_US_US_2023-01_2023-12.csv")
 cap24 = pd.read_csv("Python-Project-Milanamos-data/CAP_US_US_2024-01_2024-12.csv")
-seg23 = pd.read_csv("Python-Project-Milanamos-data/SEG_F_US_US_2023-01_2023-12.csv")
-seg24 = pd.read_csv("Python-Project-Milanamos-data/SEG_F_US_US_2024-01_2024-12.csv")
-
-# seg24.sort_values("seg_revenue_usd",ascending=False,inplace=True)
-
+seg23 = pd.read_csv("Python-Project-Milanamos-data/seg23Clean.csv")
+seg24 = pd.read_csv("Python-Project-Milanamos-data/seg24Clean.csv")
+print(len(cap23))
+# seg23.sort_values("seg_revenue_usd",ascending=False,inplace=True)
+cap23 = cap23.groupby(["origin", "destination", "operating_airline", "year", "month"], as_index=False).sum()
+cap23.to_csv('cap23Clean.csv', index=False)
 # merge the two lists and match corresponding routes and airlines and dates
-merged24 = pd.merge(cap24, seg24, how="left", left_on=['origin', 'destination', 'year', 'month'], right_on=['leg_origin', 'leg_destination', 'year', 'month'])
+#merged24 = pd.merge(cap24, seg24, how="left", left_on=['origin', 'destination', 'year', 'month'], right_on=['leg_origin', 'leg_destination', 'year', 'month'])
 # Calculate load factor
-merged24['load_factor'] = merged24['seg_passengers'] / merged24['capacity']
+##merged24['load_factor'] = merged24['seg_passengers'] / merged24['capacity']
 
-merged24 = pd.DataFrame(cap23.groupby())
+#merged24 = pd.DataFrame(cap23.groupby())
+#merged24 = cap23.groupby(["origin", "destination", "capacity"], as_index=False).sum()
 #this is incomplete
-merged24.sort_values("load_factor",ascending=False,inplace=True)
-print(len(merged24))
-print(len(merged24[merged24['load_factor'] > 1.2]))
+#merged24.sort_values("load_factor",ascending=False,inplace=True)
+#merged24.to_csv('merged24Clean.csv', index=False)
+#print(len(seg23))
+#print(len(merged24[merged24['load_factor'] > 1.2]))
+#print(merged24.head(425))
+print(len(cap23))
 #change
