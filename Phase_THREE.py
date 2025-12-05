d2c1222d5b844d2bb91b685e08f500c45daa7d5e
BASIL-NELSON <99857734+BASIL-NELSON@users.noreply.github.com>
2025-12-05 11:43:31 +0100

    added merged23Clean.csv

--- Phase_THREE.py
+++ Phase_THREE.py
@@ -12,18 +12,18 @@ seg24 = pd.read_csv("Python-Project-Milanamos-data/seg24Clean.csv")
 # seg23.sort_values("seg_revenue_usd",ascending=False,inplace=True)
 #cap23.to_csv('cap23Clean.csv', index=False)
 # merge the two lists and match corresponding routes and airlines and dates
-merged24 = pd.merge(cap24, seg24, how="left", left_on=['origin', 'destination', 'operating_airline' ,'year', 'month'], right_on=['leg_origin', 'leg_destination', 'operating_airline', 'year', 'month'])
+merged23 = pd.merge(cap23, seg23, how="left", left_on=['origin', 'destination', 'operating_airline' ,'year', 'month'], right_on=['leg_origin', 'leg_destination', 'operating_airline', 'year', 'month'])
 # Calculate load factor
-merged24['load_factor'] = merged24['seg_passengers'] / merged24['capacity']
-merged24 = merged24.replace([float('inf'), float('-inf')], pd.NA)
-merged24 = merged24.dropna()
-#merged24 = pd.DataFrame(cap23.groupby())
-#merged24 = cap23.groupby(["origin", "destination", "capacity"], as_index=False).sum()
+merged23['load_factor'] = merged23['seg_passengers'] / merged23['capacity']
+merged23 = merged23.replace([float('inf'), float('-inf')], pd.NA)
+merged23 = merged23.dropna()
+#merged23 = pd.DataFrame(cap23.groupby())
+#merged23 = cap23.groupby(["origin", "destination", "capacity"], as_index=False).sum()
 #this is incomplete
-merged24.sort_values("load_factor",ascending=False,inplace=True)
-merged24.to_csv('merged24Clean.csv', index=False)
-print(len(merged24))
-print(len(merged24[merged24['load_factor'] > 1.02]))
-print(merged24.head(320))
+merged23.sort_values("load_factor",ascending=False,inplace=True)
+merged23.to_csv('merged23Clean.csv', index=False)
+print(len(merged23))
+print(len(merged23[merged23['load_factor'] > 1.02]))
+print(merged23.head(320))
 # print(len(cap23))
 #change
